e137436be14ebcefd86976be4a7cf0445403545b
gesap <gesap9@gmail.com>
2017-01-19 13:47:07 +0900

    init

---
+++ src/main/java/TestApp.java
@@ -0,0 +1,30 @@
+import java.sql.SQLException;
+
+/**
+ * Created by gesap on 2017-01-18.
+ */
+public class TestApp {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("12323");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+
+
+
+
+
+    }
+}
