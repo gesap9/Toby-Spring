0c7c86c8ca5a921cee771cf9d8d1ae82bac13044
gesap <gesap9@gmail.com>
2017-01-23 12:13:53 +0900

    junit을 이용한 테스트

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -1,41 +1,40 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 /**
  * Created by gesap on 2017-01-19.
  */
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+    @Test
+    public  void addAndGet() throws SQLException, ClassNotFoundException {
         //XML방식
         ApplicationContext context =
                 new GenericXmlApplicationContext("applicationContext.xml");
 
-        /*
-        //Java 설정 방식
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(DaoFactory.class);
-        */
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("12323");
-
         UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("guymee");
+        user.setName("박성철");
+        user.setPassword("334423");
 
         dao.add(user);
 
-        System.out.println(user.getId() + "등록 성공");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + "조회 성공");
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(),is(user.getPassword()));
 
-
+    }
+}
+class JunitTest{
+    public static void main(String[] args) {
+        JUnitCore.main("UserDaoTest");
     }
 }
