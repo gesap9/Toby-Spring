c057c92dc1d0a0faff3631f5c53d357fa01459a6
gesap <gesap9@gmail.com>
2017-01-23 16:03:29 +0900

    junit을 이용한 테스트- addAndGet 사용자 추가 후 get 한 데이터와 비교

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
@@ -33,10 +34,6 @@ public class UserDaoTest {
         dao.add(user3);
         assertThat(dao.getCount(), is(3));
 
-
-
-
-
     }
     @Test
     public  void addAndGet() throws SQLException, ClassNotFoundException {
@@ -46,25 +43,25 @@ public class UserDaoTest {
 
 
         UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("park", "박성철", "park123");
+        User user2 = new User("lee", "이노옴", "lee123");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        User user = new User();
-        user.setId("guymee");
-        user.setName("박성철");
-        user.setPassword("334423");
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(),is(user1.getPassword()));
 
-        dao.add(user);
-        assertThat(dao.getCount(), is(1));
-
-
-        User user2 = dao.get(user.getId());
-
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(),is(user.getPassword()));
-
+        User userget2 = dao.get(user2.getId());
+        assertThat(user2.getName(), is(user2.getName()));
+        assertThat(user2.getPassword(),is(user2.getPassword()));
     }
+
+
 }
 
