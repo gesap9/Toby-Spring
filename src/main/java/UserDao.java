70c43a086c2fadbda6a2c9afdfce008ebaad548b
gesap <gesap9@gmail.com>
2017-01-24 15:52:38 +0900

    전략클래스를 클라이언트의 로컬 클래스로 선언

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -46,9 +46,21 @@ public class UserDao {
         }
     }
 
+    //Parameter를 final로 선언해줘야 한다.
+    public void add(final User user) throws ClassNotFoundException, SQLException {
+        class AddStatement implements StatementStrategy {
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(
+                        "insert into users(id, name, password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy st = new AddStatement(user);
+                return ps;
+            }
+        }
+
+        StatementStrategy st = new AddStatement();
         jdbcContextWithStatementStrategy(st);
     }
 
