d6bd0a7f280090388b512998583935d45251ff2c
gesap <gesap9@gmail.com>
2017-01-25 15:33:56 +0900

    간단한 템플릿/콜백 예제 추가  - 템플릿/콜백 재설계

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,13 +4,16 @@ import java.io.*;
  * Created by gesap on 2017-01-24.
  */
 public class Calculator {
-    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
-
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException{
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            int ret = callback.doSomethingWithReader(br);
-            return ret;
+            Integer res = initVal;
+            String line = null;
+            while((line = br.readLine()) != null){
+                res = callback.doSomethingWithLine(line,res);
+            }
+            return res;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
@@ -26,29 +29,19 @@ public class Calculator {
     }
 
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer sum = 0;
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    sum += Integer.valueOf(line);
-                }
-                return sum;
+        LineCallback sumCallback = new LineCallback() {
+            public Integer doSomethingWithLine(String line, Integer value) {
+                return value + Integer.valueOf(line);
             }
         };
-        return fileReadTemplate(filepath, sumCallback);
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
     public Integer calcMultiply(String filepath) throws IOException {
-        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
-            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                Integer multiply = 1;
-                String line = null;
-                while ((line = br.readLine()) != null) {
-                    multiply *= Integer.valueOf(line);
-                }
-                return multiply;
+        LineCallback sumCallback = new LineCallback() {
+            public Integer doSomethingWithLine(String line, Integer value) {
+                return value * Integer.valueOf(line);
             }
         };
-        return fileReadTemplate(filepath, multiplyCallback );
+        return lineReadTemplate(filepath, sumCallback, 1);
     }
 }
---
+++ src/main/java/LineCallback.java
@@ -0,0 +1,6 @@
+/**
+ * Created by gesap on 2017-01-25.
+ */
+public interface LineCallback {
+    Integer doSomethingWithLine(String line, Integer value);
+}
