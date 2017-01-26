f1e404d74056943007d850e02b3033c29a3b9caa
gesap <gesap9@gmail.com>
2017-01-26 14:45:41 +0900

    좀 더 정확한 예외 발생 확인  - DataAccessException을 DuplicateKeyException으로 변경

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -33,7 +34,7 @@ public class UserDaoTest {
         user2 = new User("lee", "이노옴", "lee123");
         user3 = new User("kim", "김하가", "kim123");
     }
-    @Test(expected = DataAccessException.class)
+    @Test(expected = DuplicateKeyException.class)
     public void duplicatedKey(){
         dao.deleteAll();
 
