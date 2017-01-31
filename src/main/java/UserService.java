4b5d64036b02eac010b7392b8fe91f2d4d87071d
gesap <gesap9@gmail.com>
2017-01-31 15:45:41 +0900

    UserService Interface로 DI 적용

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,4 +1,5 @@
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -9,7 +10,11 @@ public class UserService {
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
     UserDao userDao;
+    UserLevelUpgradePolicy userLevelUpgradePolicy;
 
+    public void setUserLevelUpgradePolicy(UserLevelUpgradePolicy userLevelUpgradePolicy) {
+        this.userLevelUpgradePolicy = userLevelUpgradePolicy;
+    }
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
@@ -17,33 +22,10 @@ public class UserService {
     public void upgradeLevels() {
         List<User> users = userDao.getAll();
         for (User user : users) {
-            if (canUpgradeLevel(user)) {
-                upgradeLevel(user);
+            if (userLevelUpgradePolicy.canUpgradeLevel(user)) {
+                userLevelUpgradePolicy.upgradeLevel(user);
             }
-
         }
-
-    }
-
-    private boolean canUpgradeLevel(User user) {
-        Level currentLevel = user.getLevel();
-
-        switch (currentLevel) {
-            case BASIC:
-                return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
-            case SILVER:
-                return (user.getRecommend() >= MIN_RECOMMEND_FOR_GOLD);
-            case GOLD:
-                return false;
-                default: throw new IllegalArgumentException("Unknown Level : " +
-                        currentLevel);
-        }
-    }
-
-    private void upgradeLevel(User user) {
-        user.upgradeLevel();
-        userDao.update(user);
-
     }
 
     public void add(User user) {
