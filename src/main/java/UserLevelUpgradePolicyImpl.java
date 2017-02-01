4dcba6baa82938cfd978cfce09ec0711e363ea43
gesap <gesap9@gmail.com>
2017-02-01 10:21:10 +0900

    Spring Mailsender 이용 DI 처리

--- src/main/java/UserLevelUpgradePolicyImpl.java
+++ src/main/java/UserLevelUpgradePolicyImpl.java
@@ -1,4 +1,7 @@
 
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
 import javax.mail.Message;
 import javax.mail.Message.RecipientType;
 import javax.mail.MessagingException;
@@ -18,6 +21,11 @@ public class UserLevelUpgradePolicyImpl implements UserLevelUpgradePolicy {
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
 
     UserDao userDao;
+    private MailSender mailSender;
+
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
@@ -47,25 +55,14 @@ public class UserLevelUpgradePolicyImpl implements UserLevelUpgradePolicy {
     }
 
     private void sendUpgradeEMail(User user) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "mail.ksug.org");
-        Session s = Session.getInstance(props, null);
 
-        MimeMessage message = new MimeMessage(s);
-        try{
-            message.setFrom(new InternetAddress("ddd@mail.com"));
-            message.addRecipients(RecipientType.TO, user.getEmail());
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("user@mail.com");
+        mailMessage.setSubject("Inform Upgrade");
+        mailMessage.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
 
-            message.setSubject("Upgrade 안내");
-            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
+        this.mailSender.send(mailMessage);
 
-            Transport.send(message);
-
-        } catch (AddressException e) {
-            throw new RuntimeException(e);
-
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
