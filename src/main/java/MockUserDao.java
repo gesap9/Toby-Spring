45b09429bbbe62d44dc7d1c93ea305ea32a7fae7
gesap <gesap9@gmail.com>
2017-02-01 16:44:03 +0900

    고립된 단위테스트 UserDao를 구현한 목 오브젝트를 활용하여 테스트

---
+++ src/main/java/MockUserDao.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gesap on 2017-02-01.
+ */
+public class MockUserDao implements UserDao {
+
+    private List<User> users;
+    private List<User> updated = new ArrayList<User>();
+
+    public MockUserDao(List<User> users) {
+        this.users = users;
+    }
+
+    public List<User> getUpdated() {
+        return updated;
+    }
+
+    public List<User> getAll() {
+        return this.users;
+    }
+
+    public void update(User user) {
+        updated.add(user);
+    }
+
+    public void add(User user) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void deleteAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getCount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public User get(String id) {
+        throw new UnsupportedOperationException();
+    }
+
+
+}
