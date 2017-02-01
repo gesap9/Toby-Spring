03c3dc973a50b7f5e911a1a000d6c2f7e923383d
gesap <gesap9@gmail.com>
2017-02-01 11:25:54 +0900

    MockObject를 이용하여 메일전송여부 테스트

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -36,10 +37,10 @@ public class UserServiceTest {
     public void setUp() {
         users = Arrays.asList(
                 new User("bumjin", "박범진", "p1", Level.BASIC, UserService.MIN_LOGCOUNT_FOR_SILVER - 1, 0, "bumjin@mail.com"),
-                new User("joytouch", "강명성", "p2", Level.BASIC, UserService.MIN_LOGCOUNT_FOR_SILVER, 0,"joytouch@mail.com"),
-                new User("erwins", "신승한", "p3", Level.SILVER, 60, UserService.MIN_RECOMMEND_FOR_GOLD - 1,"erwins@mail.com"),
-                new User("madnite1", "이상호", "p4", Level.SILVER, 60, UserService.MIN_RECOMMEND_FOR_GOLD,"madnitel1@mail.com"),
-                new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE,"green@mail.com")
+                new User("joytouch", "강명성", "p2", Level.BASIC, UserService.MIN_LOGCOUNT_FOR_SILVER, 0, "joytouch@mail.com"),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, UserService.MIN_RECOMMEND_FOR_GOLD - 1, "erwins@mail.com"),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, UserService.MIN_RECOMMEND_FOR_GOLD, "madnitel1@mail.com"),
+                new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE, "green@mail.com")
 
         );
     }
@@ -84,9 +85,16 @@ public class UserServiceTest {
     }
 
     @Test
+    @DirtiesContext
     public void upgradeLevels() throws Exception {
         userDao.deleteAll();
         for (User user : users) userDao.add(user);
+
+        MockMailSender mockMailSender = new MockMailSender();
+        UserLevelUpgradePolicyImpl userLevelUpgradePolicy = new UserLevelUpgradePolicyImpl();
+        userLevelUpgradePolicy.setUserDao(userDao);
+        userLevelUpgradePolicy.setMailSender(mockMailSender);
+        userService.setUserLevelUpgradePolicy(userLevelUpgradePolicy);
 
         userService.upgradeLevels();
 
@@ -96,6 +104,10 @@ public class UserServiceTest {
         checkLevelUpgraded(users.get(3), true);
         checkLevelUpgraded(users.get(4), false);
 
+        List<String> request = mockMailSender.getRequests();
+        assertThat(request.size(), is(2));
+        assertThat(request.get(0),is(users.get(1).getEmail()));
+        assertThat(request.get(1),is(users.get(3).getEmail()));
 
     }
 
