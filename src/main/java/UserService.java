43f7317db76d8c018548826e87897407ad48ec27
gesap <gesap9@gmail.com>
2017-02-28 16:53:51 +0900

    트랜잭션 애노테이선 사용

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,11 +1,16 @@
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 /**
  * Created by gesap on 2017-02-01.
  */
+@Transactional
 public interface UserService {
     void add(User user);
+    @Transactional(readOnly = true)
     User get(String id);
+    @Transactional(readOnly = true)
     List<User> getAll();
     void deleteAll();
     void update(User user);
