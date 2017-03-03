2b3381e8ec79b50728cbc77abd7c7be425dc0a2b
gesap <gesap9@gmail.com>
2017-03-03 14:36:21 +0900

    @PostConstruct를 이용하여 빈 초기화 처리

--- src/main/java/springbook/XmlSqlService.java
+++ src/main/java/springbook/XmlSqlService.java
@@ -1,5 +1,6 @@
 package springbook;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -12,13 +13,23 @@ import java.util.Map;
  */
 public class XmlSqlService implements SqlService {
     private Map<String, String> sqlMap = new HashMap<>();
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
 
     public XmlSqlService() {
+
+    }
+
+    @PostConstruct
+    public void loadSql() {
         String contextPath = Sqlmap.class.getPackage().getName();
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = UserDao.class.getResourceAsStream("../sqlmap.xml");
+            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for (SqlType sql : sqlmap.getSql()) {
