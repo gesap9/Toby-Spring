d32167aee1dc80f0788fd27515a82fbb3298a727
gesap <gesap9@gmail.com>
2017-03-06 09:56:07 +0900

    디폴트 의존관계

--- src/main/java/springbook/JaxbXmlSqlReader.java
+++ src/main/java/springbook/JaxbXmlSqlReader.java
@@ -9,7 +9,8 @@ import java.io.InputStream;
  * Created by gesap on 2017-03-06.
  */
 public class JaxbXmlSqlReader implements SqlReader {
-    private String sqlmapFile;
+    private static final String DEFAULT_SQLMAP_FILE = "../sqlmap.xml";
+    private String sqlmapFile = DEFAULT_SQLMAP_FILE;
 
     public void setSqlmapFile(String sqlmapFile) {
         this.sqlmapFile = sqlmapFile;
