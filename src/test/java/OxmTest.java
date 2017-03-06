31ffaf999cc62e44225848c82dac8b082ddd4471
gesap <gesap9@gmail.com>
2017-03-06 10:14:45 +0900

    Spring의 OXM 추상화 서비스 인터페이스를 이용해서 XML을 자바오브젝트로 변환  - JAXB 구현체 이용 학습 테스트 구현

---
+++ src/test/java/OxmTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.SqlType;
+import springbook.Sqlmap;
+
+import org.springframework.oxm.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by gesap on 2017-03-06.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class OxmTest {
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshallSqlMap() throws XmlMappingException, IOException{
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("sqlmap.xml"));
+        Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+        List<SqlType> sqlList = sqlmap.getSql();
+        assertThat(sqlList.size(),is(6));
+    }
+}
