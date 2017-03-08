a808171819181ed7ebf2f04eb947780e677cd381
gesap <gesap9@gmail.com>
2017-03-08 10:49:34 +0900

    내장형 DB (HSQL) 적용  - 트랜잭션 적용

--- src/main/java/springbook/EmbeddedDbSqlRegistry.java
+++ src/main/java/springbook/EmbeddedDbSqlRegistry.java
@@ -2,6 +2,10 @@ package springbook;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.util.Map;
@@ -11,23 +15,28 @@ import java.util.Map;
  */
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     SimpleJdbcTemplate jdbc;
+    TransactionTemplate transactionTemplate;
 
-    public void setDataSource(DataSource dataSource){
-        jdbc= new SimpleJdbcTemplate(dataSource);
+    public void setDataSource(DataSource dataSource) {
+
+        jdbc = new SimpleJdbcTemplate(dataSource);
+        transactionTemplate = new TransactionTemplate(
+                new DataSourceTransactionManager(dataSource)
+        );
     }
 
     @Override
     public void registerSql(String key, String sql) {
 
-        jdbc.update("insert into sqlmap(key_, sql_) values(?,?)", key,sql);
+        jdbc.update("insert into sqlmap(key_, sql_) values(?,?)", key, sql);
 
     }
 
     @Override
     public String findSql(String key) throws SqlNotFoundException {
-        try{
+        try {
             return jdbc.queryForObject("select sql_ from sqlmap where key_ = ?", String.class, key);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             throw new SqlNotFoundException(key + "에 해당하는 SQL을 찾을 수 없습니다.", e);
 
         }
@@ -36,16 +45,22 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     @Override
     public void updateSql(String key, String sql) throws SqlUpdateFailureException {
         int affected = jdbc.update("update sqlmap set sql_ = ? where key_ = ?", sql, key);
-        if(affected == 0){
+        if (affected == 0) {
             throw new SqlUpdateFailureException(key + "에 해당하는 SQL을 찾을 수 없습니다.");
 
         }
     }
 
     @Override
-    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-       for(Map.Entry<String, String> entry:sqlmap.entrySet()){
-           updateSql(entry.getKey(), entry.getValue());
-       }
+    public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+                    updateSql(entry.getKey(), entry.getValue());
+                }
+            }
+        });
+
     }
 }
--- src/test/java/EmbeddedDbSqlRegistryTest.java
+++ src/test/java/EmbeddedDbSqlRegistryTest.java
@@ -1,9 +1,16 @@
 import org.junit.After;
+import org.junit.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import springbook.EmbeddedDbSqlRegistry;
+import springbook.SqlUpdateFailureException;
 import springbook.UpdatableSqlRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.fail;
 
 /**
  * Created by gesap on 2017-03-07.
@@ -21,6 +28,24 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
         embeddedDbSqlRegistry.setDataSource(db);
 
         return embeddedDbSqlRegistry;
+    }
+
+    @Test
+    public void transactionUpdate(){
+        checkFindResult("SQL1", "SQL2", "SQL3");
+        Map<String, String> sqlmap= new HashMap<>();
+        sqlmap.put("KEY1","Modified1");
+        sqlmap.put("KEY2323122", "Modifiedd33");
+
+        try{
+            sqlRegistry.updateSql(sqlmap);
+            fail();
+        }catch (SqlUpdateFailureException e){}
+
+        checkFindResult("SQL1", "SQL2", "SQL3");
+
+
+
     }
 
     @After
